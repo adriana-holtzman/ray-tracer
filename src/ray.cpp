93ebfd0013ceae237e6472b9c47e9e3480497659
Adriana <adrianaholtzman6@gmail.com>
2026-02-04 18:34:45 +0100

    ray class implemented

--- src/ray.cpp
+++ src/ray.cpp
@@ -1,12 +1,13 @@
 #include "ray.h"
 
-// TODO: initialize origin and direction to zero vectors
+// Initialize
 Ray::Ray()
     : orig(Vec3(0.0, 0.0, 0.0)), dir(Vec3(0.0, 0.0, 0.0)) {}
 
 Ray::Ray(const Vec3& origin, const Vec3& direction)
     : orig(origin), dir(direction) {}
-    
+
+// Return origin or direction methods
 const Vec3& Ray::origin() const {
     return orig;
 }
@@ -15,7 +16,7 @@ const Vec3& Ray::direction() const {
     return dir;
 }
 
+// at method, origin + time * direction
 Vec3 Ray::at(double t) const {
-    // TODO: origin + t * direction
-    return orig;
+    return orig + dir * t;
 }
