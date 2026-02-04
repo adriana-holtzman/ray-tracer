5e3d1e4b42a90e802918b6f7534c58d2709aeb0f
Adriana <adrianaholtzman6@gmail.com>
2026-02-04 18:29:00 +0100

    vec3 class implemented

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -1,37 +1,51 @@
 #include "vec3.h"
 #include <cmath>
 
-// TODO: initialize to (0,0,0)
+// Initialize to 0,0,0 if no values given
 Vec3::Vec3() : e{0.0, 0.0, 0.0} {}
 
+// Initialize to x,y,z if values given
 Vec3::Vec3(double x, double y, double z) : e{x, y, z} {}
 
+// Return component
 double Vec3::x() const { return e[0]; }
 double Vec3::y() const { return e[1]; }
 double Vec3::z() const { return e[2]; }
 
-// TODO: implement vector arithmetic
+// Addition
 Vec3 Vec3::operator+(const Vec3& other) const {
-    return Vec3();
+    return Vec3(e[0] + other.e[0],
+                e[1] + other.e[1],
+                e[2] + other.e[2]);
 }
 
+// Subtraction
 Vec3 Vec3::operator-(const Vec3& other) const {
-    return Vec3();
+    return Vec3(e[0] - other.e[0], 
+                e[1] - other.e[1], 
+                e[2] - other.e[2]);
 }
 
+// Scalar multiplication
 Vec3 Vec3::operator*(double s) const {
-    return Vec3();
+    return Vec3(e[0] * s, 
+                e[1] * s, 
+                e[2] * s);
 }
 
+// Scalar division
 Vec3 Vec3::operator/(double s) const {
-    return Vec3();
+    return Vec3(e[0] / s, 
+                e[1] / s, 
+                e[2] / s);
 }
 
-// TODO: implement length and normalization
+// Length 
 double Vec3::length() const {
-    return 0.0;
+    return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
 }
 
+// Normalization
 Vec3 Vec3::normalized() const {
-    return Vec3();
+    return *this/length();
 }
